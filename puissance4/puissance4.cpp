ceced2f3b903219b3aa29492fd2df8b9d880bb5f
onurt91 <onurtas91240@gmail.com>
2016-03-23 19:55:54 +0100

    Update puissance4.cpp
    là c'est le bon avec se que je t'es raconté en classe ( le truk qui test pas tout) (OPTIMISE)

--- puissance4/puissance4.cpp
+++ puissance4/puissance4.cpp
@@ -67,67 +67,93 @@ void Puissance4::changementJoueur()
 int Puissance4::detectionFinDePartie()
 {
 
-    //On test les lignes
-    for (int j = 0; j < 6; ++j)
+//horizontal
+
+if (grille[i+3][j] == grille[i+2][j] && grille[i+2][j] == grille[i+1][j] && grille[i+1][j] == grille[i][j] && grille[i][j] != 0)
     {
-        for (int i = 0; i < 3; ++i)
-        {
-            if (grille[i][j] == grille[i+1][j] && grille[i+1][j] == grille[i+2][j] && grille[i+2][j] == grille[i+3][j] && grille[i][j] != 0)
-            {
-                return grille[i][j];
-            }
-        }
+        return grille[i][j];
     }
 
-    //On test les colonnes
-    for (int i = 0; i < 7; ++i)
+if (grille[i+2][j] == grille[i+1][j] && grille[i+1][j] == grille[i][j] && grille[i][j] == grille[i-1][j] && grille[i][j] != 0)
     {
-        for (int j = 0; j < 2; ++i)
-        {
-            if (grille[i][j] == grille[i][j+1] && grille[i][j+1] == grille[i][j+2] && grille[i][j+2] == grille[i][j+3] && grille[i][j] != 0)
-            {
-                return grille[i][j];
-            }
-        }
+        return grille[i][j];
     }
 
-    //On test les diagonnales
-    if (grille[0][2] == grille[1][3] && grille[1][3] == grille[2][4] && grille[2][4] == grille[3][5] && grille[0][3] != 0)
+if (grille[i+1][j] == grille[i][j] && grille[i][j] == grille[i-1][j] && grille[i-1][j] == grille[i-2][j] && grille[i][j] != 0)
     {
-        return grille[0][3];
-    }
-    if (grille[3][0] == grille[4][1] && grille[4][1] == grille[5][2] && grille[5][2] == grille[6][3] && grille[3][0] != 0)
-    {
-        return grille[3][0];
-    }
-    for (int i = 0; i < 2; ++i)
-    {
-        if (grille[0+i][1+i] == grille[1+i][2+i] && grille[1+i][2+i] == grille[2+i][3+i]
-                && grille[2+i][3+i] == grille[3+i][4+i] && grille[3+i][4+i] == grille[4+i][5+i] && grille[2][3] != 0)
-        {
-            return grille[2][3];
-        }
-    }
-    for (int i = 0; i < 3; ++i)
-    {
-        if (grille[0+i][0+i] == grille[1+i][1+i] && grille[1+i][1+i] == grille[2+i][2+i]
-                && grille[2+i][2+i] == grille[3+i][3+i] && grille[3+i][3+i] == grille[4+i][4+i] && grille[3][3] != 0)
-        {
-            return grille[3][3];
-        }
-    }
-    for (int i = 0; i < 4; ++i)
-    {
-        if (grille[2+i][0+i] == grille[3+i][1+i] && grille[3+i][1+i] == grille[4+i][2+i]
-                && grille[4+i][2+i] == grille[5+i][3+i] && grille[5+i][3+i] == grille[6+i][4+i] && grille[4][2] != 0)
-        {
-            return grille[4][2];
-        }
+        return grille[i][j];
     }
 
-    //************************************//
-    //Rajouter les diago dans l'autre sens//
-    //************************************//
+if (grille[i][j] == grille[i-1][j] && grille[i-1][j] == grille[i-2][j] && grille[i-2][j] == grille[i-3][j] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+//verticale
+
+if (grille[i][j+3] == grille[i][j+2] && grille[i][j+2] == grille[i][j+1] && grille[i][j+1] == grille[i][j] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i][j+2] == grille[i][j+1] && grille[i][j+1] == grille[i][j] && grille[i][j] == grille[i][j-1] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i][j+1] == grille[i][j] && grille[i][j] == grille[i][j-1] && grille[i][j-1] == grille[i][j-2] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i][j] == grille[i][j-1] && grille[i][j-1] == grille[i][j-2] && grille[i][j-2] == grille[i][j-3] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+//f(x)=-x
+
+if (grille[i-3][j+3] == grille[i-2][j+2] && grille[i-2][j+2] == grille[i-1][j+1] && grille[i-1][j+1] == grille[i][j] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i-2][j+2] == grille[i-1][j+1] && grille[i-1][j+1] == grille[i][j] && grille[i][j] == grille[i+1][j-1] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i-1][j+1] == grille[i][j] && grille[i][j] == grille[i+1][j-1] && grille[i+1][j-1] == grille[i+2][j-2] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    } 
+
+if (grille[i][j] == grille[i+1][j-1] && grille[i+1][j-1] == grille[i+2][j-2] && grille[i+2][j-2] == grille[i+3][j-3] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+//f(x)=x
+
+if (grille[i+3][j+3] == grille[i+2][j+2] && grille[i+2][j+2] == grille[i+1][j+1] && grille[i+1][j+1] == grille[i][j] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i+2][j+2] == grille[i+1][j+1] && grille[i+1][j+1] == grille[i][j] && grille[i][j] == grille[i-1][j-1] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }    
+
+if (grille[i+1][j+1] == grille[i][j] && grille[i][j] == grille[i-1][j-1] && grille[i-1][j-1] == grille[i-2][j-2] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
+
+if (grille[i][j] == grille[i-1][j-1] && grille[i-1][j-1] == grille[i-2][j-2] && grille[i-2][j-2] == grille[i-3][j-3] && grille[i][j] != 0)
+    {
+        return grille[i][j];
+    }
 
     return 0;
 }
